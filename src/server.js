e133e999f0a19b718d694f934e0b6387165bf25a
yuriDuque <yurithielmann83@gmail.com>
2021-04-27 20:44:51 -0300

    enviando ultimas alteracoes

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 dotenv.config();
-console.log(`.env configurado ${process.env.APP_URL}`); // 8626
+console.log(`.env configurado`);
 
 // define que a comunicacao do express sera por defaul json
 app.use(express.json()); 
@@ -17,3 +17,6 @@ app.get("/teste", (req, res) => {res.json({mensagem: "A api está rodando!"})})
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {console.log(`sua aplicação está rodando na porta: ${port}`)});
+
+console.log(`sua aplicacao está rodando: ${process.env.APP_URL}:${port}`);
+
