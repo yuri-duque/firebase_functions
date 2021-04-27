b7ac328aad166a61bd552325bd19106d121f5c06
yuriDuque <yurithielmann83@gmail.com>
2021-04-27 19:37:44 -0300

    enviando api vazia

--- src/server.js
+++ src/server.js
@@ -5,5 +5,7 @@ const app = express();
 // define que a comunicacao do express sera por defaul json
 app.use(express.json()); 
 
+app.get("/teste", (req, res) => {res.json({mensagem: "A api está rodando!"})})
+
 // manda o express escutar a porta 3333
 app.listen(3333, () => {console.log("Server is running on port 3333")});
