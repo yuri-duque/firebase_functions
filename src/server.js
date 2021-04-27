8a2076c9fe893731d15bad98cfa8df7df25c5e40
yuriDuque <yurithielmann83@gmail.com>
2021-04-27 20:34:39 -0300

    enviando ultimas alteracoes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 
+dotenv.config();
+console.log(`Your app url is ${process.env.APP_URL}`); // 8626
+
 // define que a comunicacao do express sera por defaul json
 app.use(express.json()); 
+app.use(cors()); 
 
 app.get("/teste", (req, res) => {res.json({mensagem: "A api está rodando!"})})
 
 // manda o express escutar a porta 3333
-app.listen(5000, () => {console.log("Server is running on port 5000")});
+app.listen(3000, () => {console.log("Server is running on port 5000")});
